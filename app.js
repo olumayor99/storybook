7edf7336b9674951170269a9707c9b479182c306
olumayor99 <32824520+olumayor99@users.noreply.github.com>
2020-06-24 23:12:29 +0300

    added login and sessions, made navbar, added middleware to specify views according to login status

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const connectDB = require("./config/db");
 
 //Load config
 dotenv.config({ path: "./config/config.env" });
+
+//Passport config
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -18,6 +26,28 @@ if (process.env.NODE_ENV === "development") {
 
 //Handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+//Sessions
+app.use(
+  session({
+    secret: "kratos",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//Routes
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 
---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+
+// @desc Google Auth
+// @route GET /auth/google
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+// @desc Google Auth callback
+// @route GET /auth/google/callback
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => res.redirect("/dashboard")
+);
+
+// @desc Google Logout
+// @route GET /auth/logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+module.exports = router;
