30b00dc719be3cccd24c6b0feeb3b9e9ac845408
olumayor99 <32824520+olumayor99@users.noreply.github.com>
2020-06-30 00:17:35 +0300

    fixed delete button

--- routes/stories.js
+++ routes/stories.js
@@ -88,7 +88,7 @@ router.put("/:id", ensureAuth, async (req, res) => {
 
 // @desc Delete story
 // @route DELETE /stories/:id
-router.get("/:id", ensureAuth, async (req, res) => {
+router.delete("/:id", ensureAuth, async (req, res) => {
   try {
     await Story.remove({ _id: req.params.id });
     res.redirect("/dashboard");
