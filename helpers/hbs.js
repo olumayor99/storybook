7caf087f8761565bc6952c38520ec66bad6056ab
olumayor99 <32824520+olumayor99@users.noreply.github.com>
2020-06-30 00:09:06 +0300

    made edit and delete functionality

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -19,6 +19,7 @@ module.exports = {
   stripTags: (input) => {
     return input.replace(/<(?:.|\n)*?>/gm, "");
   },
+
   editIcon: (storyUser, loggedUser, storyId, floating = true) => {
     if (storyUser._id.toString() == loggedUser._id.toString()) {
       if (floating) {
@@ -29,5 +30,15 @@ module.exports = {
     } else {
       return "";
     }
+  },
+
+  select: (selected, options) => {
+    return options
+      .fn(this)
+      .replace(new RegExp('value="' + selected + '"'), '$& selected="selected"')
+      .replace(
+        new RegExp(">" + selected + "</option>"),
+        ' selected="selected"$&'
+      );
   },
 };
